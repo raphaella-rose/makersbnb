47ab1573b00faf7912b80ceb075913435caf2821
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-06-16 09:10:43 +0100

    user can navigate to login page

--- spec/features/login_or_signup_spec.rb
+++ spec/features/login_or_signup_spec.rb
@@ -6,4 +6,12 @@ feature 'user can choose to login or sign up' do
 
     expect(page).to have_content 'Please enter your details to sign up!'
   end
+
+  scenario 'registered user navigates to log in page' do
+
+    visit('/makersbnb/index')
+    click_link('Log in')
+
+    expect(page).to have_content 'Login here:'
+  end
 end
