9ffad6f889ad8c94b3614417e841bca252598d86
Raphaella <raphaella@Raphaellas-MacBook-Air.local>
2022-06-17 13:52:51 +0100

    updated routing for user sign up

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,7 +1,6 @@
 feature 'registration' do
   scenario 'a user can sign up' do
     visit '/makersbnb/index'
-    click_link('Sign Up')
     fill_in('email', with: 'test@example.com')
     fill_in('password', with: 'password123')
     click_button('Submit')
