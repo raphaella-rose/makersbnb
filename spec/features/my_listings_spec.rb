f3c7d6d9834d7795f8517741dbc3adc54e4db160
Patrick Reynolds <patrickreynolds@Patricks-MacBook-Air.local>
2022-06-17 13:08:55 +0100

    repush of last commit

--- spec/features/my_listings_spec.rb
+++ spec/features/my_listings_spec.rb
@@ -1,18 +1,19 @@
 feature "my_listings page" do
   scenario "the view more details page takes you to the properties page" do
-    log_in
+    log_in #add in sign_up method above log_in after merging
     visit '/makersbnb/my_listings' #delete this line after raphaella's refactoring
     click_button('View more details')
     expect(page).to have_content "Privet Drive"
   end
+  
   scenario "a property is added, user clicks on property on my listing page, recieves description" do
-    log_in
+    log_in #add in sign_up method above log_in after merging
     visit '/makersbnb/add' 
     fill_in 'property_title', with: "Paddys house"
     fill_in 'description', with: "the nicest place in all of london"
     fill_in 'price_per_night', with: 2000
     click_button('Submit')
-    find_by_id("Paddy's house").click
+    find_by_id("Paddys house").click
     expect(page).to have_content "the nicest place in all of london"
   end
 
